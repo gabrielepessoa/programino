a0bf9f8fd7c5338364df1362766714d6336011d9
gabrielepessoa <gabriele.pessoa@gmail.com>
2019-12-01 17:27:25 -0300

    Adding total result

--- __main__.py
+++ __main__.py
@@ -18,7 +18,7 @@ ai_test_number = int(sys.argv[2])
 # com os resultados das simulações
 now = datetime.now()
 base_path = os.path.dirname(os.path.abspath(__file__))
-base_file_name = base_path + '/experiments/{:%Y%m%d_%Hh%M}'.format(now)
+base_file_name = base_path + '/experiments/{:%Y%m%d_%H%M%S}'.format(now)
 random_file_name = base_file_name + '_random.csv'
 ai_file_name = base_file_name + '_ai.csv'
 
@@ -26,11 +26,12 @@ ai_file_name = base_file_name + '_ai.csv'
 print("\nO experimento será executado da seguinte forma:")
 print("({}) jogos de jogadas aleatórias x jogadas aleatórias".format(random_test_number))
 print("({}) jogos de jogadas aleatórias x jogadas com minimax\n".format(ai_test_number))
-time.sleep(2)  # REMOVER AO EXECUTAR GRANDES EXPERIMENTOS
+# time.sleep(2)  # REMOVER AO EXECUTAR GRANDES EXPERIMENTOS
 
 
 # Iniciando a simulação de (Aleatório x Aleatório)
 random_test_results = []
+total_random_test_result = [0, 0, 0]
 for count in range(0, random_test_number):
     print("(Aleatório x Aleatório) Jogo {}:".format(count+1))
 
@@ -41,8 +42,10 @@ for count in range(0, random_test_number):
 
     result = game.get_result()
     random_test_results.append(result)
+    total_random_test_result[result.index(1)] += 1
+
     print(result, end='\n\n')
-    time.sleep(1)  # REMOVER AO EXECUTAR GRANDES EXPERIMENTOS
+    # time.sleep(1)  # REMOVER AO EXECUTAR GRANDES EXPERIMENTOS
 
 print("Resultado da experiência: ", random_test_results, end='\n\n')
 
@@ -51,10 +54,12 @@ with open(random_file_name, 'w') as random_experiment:
     writer = csv.writer(random_experiment, delimiter=';')
     writer.writerow(["player0", "player1", "stuck"])
     writer.writerows(random_test_results)
+    writer.writerow(total_random_test_result)
 
 
 # Iniciando a simulação de (Aleatório x Minimax)
 ai_test_results = []
+total_ai_test_result = [0, 0, 0]
 for count in range(0, ai_test_number):
     print("(Aleatório x Minimax) Jogo {}:".format(count + 1))
 
@@ -69,6 +74,8 @@ for count in range(0, ai_test_number):
 
     result = game.get_result()
     ai_test_results.append(result)
+    total_ai_test_result[result.index(1)] += 1
+
     print(result, end='\n\n')
 
 print("Resultado da experiência: ", ai_test_results, end='\n\n')
@@ -78,6 +85,7 @@ with open(ai_file_name, 'w') as ai_experiment:
     writer = csv.writer(ai_experiment, delimiter=';')
     writer.writerow(["player0", "player1", "stuck"])
     writer.writerows(ai_test_results)
+    writer.writerow(total_ai_test_result)
 
 
 print("DONE!")
